0d2f49998abb42dba9b8a2f7c40bfa5c0a5596f8
apousette <adam021@live.se>
2021-10-08 14:26:09 +0200

    alive and running

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const indexRoute = require('./routes/indexRoute');
+
+const port = 8080;
+
+app.use(express.static('public'));
+app.use('/', indexRoute);
+app.listen(port);
+
+console.log('Sever is up and running listening to port ' + port);
+
+
